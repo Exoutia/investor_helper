44bb1afd1d462ce1d81b5d5aa2cb36587e852bad
Exoutia <jhav365@gmail.com>
2023-09-18 21:24:46 +0530

    creating styles and htmx to the application.

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
+from flask_assets import Bundle, Environment
 from flask_sqlalchemy import SQLAlchemy
 import json
+import requests
+import time
 from mftool import Mftool
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mutualFund.db"
 db = SQLAlchemy(app)
+
+api_url = "https://api.mfapi.in/mf/"
+
+# assets
+assets = Environment(app)
+css = Bundle("src/main.css", output="dist/main.css")
+
+assets.register("css", css)
+css.build()
 
 
 class MutualFund(db.Model):
@@ -19,11 +31,17 @@ class MutualFund(db.Model):
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    spinner = "static/svg/svg-loaders/spinning-circles.svg"
+    return render_template("index.html" , spinner=spinner)
 
+@app.route("/search-bar")
+def search_bar():
+    spinner = "static/svg/svg-loaders/spinning-circles.svg"
+    return render_template("search-bar.html", spinner=spinner)
 
 @app.route("/search")
 def search():
+    time.sleep(5)
     query = request.args.get("query")
     if query:
         mutual_funds = (
@@ -34,17 +52,26 @@ def search():
             .limit(100)
             .all()
         )
+        if not mutual_funds:
+            mutual_funds = ""
     else:
         mutual_funds = MutualFund.query.limit(100).all()
-    return render_template("search.html", results=mutual_funds)
+        
+    spinner = "static/svg/svg-loaders/spinning-circles.svg"
+    return render_template("search-results.html", results=mutual_funds, spinner=spinner)
 
 
 @app.route("/mutualfunds/<scheme_code>")
 def funds_detials(scheme_code):
-    mf = Mftool()
-    mutual_fund_result = mf.get_scheme_quote(scheme_code)
-    print(mutual_fund_result)
-    return render_template("mutualfundsDetails.html", result=mutual_fund_result)
+    response = requests.get(api_url + scheme_code + "/latest")
+    if response.status_code == 200:
+        mutual_fund_result = response.json()
+    else: 
+        mutual_fund_result = None
+        
+    spinner = "static/svg/svg-loaders/spinning-circles.svg"
+    # print("hi:", mutual_fund_result)
+    return render_template("mutualfundsDetails.html", result=mutual_fund_result, spinner=spinner)
 
 
 def load_data():
